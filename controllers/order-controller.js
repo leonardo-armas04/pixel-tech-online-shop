02ee96f2e1c01fe0455fa91333e7d500ed208fc0
Leonardo <leo.armas.rs@gmail.com>
2025-10-01 12:06:22 -0600

    added payment method via stripe

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,3 +1,5 @@
+// const stripe = require('stripe')("some-kind-of-key")
+
 const Order = require("../models/order-model")
 const User = require("../models/user-model")
 
@@ -32,10 +34,39 @@ async function addOrder(req,res,next) {
     }
 
     req.session.cart = null
-    res.redirect("/orders")
+    
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map((item) => {
+      return {
+        price_data: {
+          currency: 'mxn',
+          product_data: {
+            name: item.product.title,
+          },
+          unit_amount: +item.product.price * 100,
+        },
+        quantity: +item.quantity,
+      }
+    }),
+    mode: 'payment',
+    success_url: 'http://localhost:4040/orders/success',
+    cancel_url: 'http://localhost:4040/orders/cancel',
+  });
+
+  res.redirect(303, session.url)
+}
+
+function getSuccess(req,res) {
+  res.render("customer/orders/success")
+}
+
+function getCancel(req,res) {
+  res.render("customer/orders/cancel")
 }
 
 module.exports = {
     getOrders: getOrders,
-    addOrder: addOrder
+    addOrder: addOrder,
+    getSuccess: getSuccess,
+    getCancel: getCancel
 }
--- routes/orders-routes.js
+++ routes/orders-routes.js
@@ -6,6 +6,7 @@ const orderController = require("../controllers/order-controller")
 // 127.0.0.1:4040/orders/...
 router.post("/",orderController.addOrder)
 router.get("/",orderController.getOrders)
-
+router.get("/success",orderController.getSuccess)
+router.get("/cancel",orderController.getCancel)
 
 module.exports = router
